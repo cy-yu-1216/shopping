e8b44dce7932b28ed23c9b056e5c9c4df8944183
chenyu <ChenYu9991216@hotmail.com>
2021-05-09 18:04:56 +0800

    第二次提交（底部导航）

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tab-bar-item">
+    <div v-if="isActive" @click="itemclick">
+        <slot name="item-ico"></slot>
+    </div>
+    <div v-else>
+       <slot name="item-active-ico"></slot>
+    </div>
+    <div :style="isText">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path:String
+  },
+  props: {
+    path: String,
+    activeColor:{
+      type:String,
+      default:'red'
+    }
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.indexOf(this.path) == -1
+    },
+    isText() {
+      return this.isActive ? {} : {color: this.activeColor}
+    }
+  },
+    methods: {
+      itemclick() {
+        this.$router.replace(this.path);
+
+      }
+
+    }
+}
+</script>
+
+<style>
+.tab-bar-item{
+  flex:1;
+  text-align: center;
+  font-size: 14px;
+  height: 49px;
+
+}
+.tab-bar-item img{
+  width: 24px;
+  height: 24px;
+  vertical-align: middle;
+  margin-top: 2px;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,47 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+// import Home from '../views/Home.vue'
+const Home =()=>import("views/home/Home.vue")
+const Profile =()=>import("views/profile/Profile.vue")
+const Cart =()=>import("views/cart/Cart.vue")
+const Category =()=>import("views/category/Category.vue")
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/category',
+
+    component: Category
+  },
+  {
+    path: '/cart',
+
+    component: Cart
+  },
+  {
+    path: '/profile',
+
+    component: Profile
   }
 ]
-
+// //获取原型对象上的push函数
+// const originalPush = VueRouter.prototype.replace
+// //修改原型对象中的push方法
+// VueRouter.prototype.replace = function replace(location) {
+//   return originalPush.call(this, location).catch(err => err)
+// }
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+
   routes
 })
 
